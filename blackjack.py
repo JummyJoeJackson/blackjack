977c70922ee4cb8fdb287e1ef49430442869e573
Diego Gonzalez <116301465+JummyJoeJackson@users.noreply.github.com>
2023-06-04 15:39:58 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,10 @@
 #importing functions
-import random
+from random import randrange
 
 #assign all possible card suits and values
 cardSuits = ["Spades","Clubs","Diamonds","Hearts"]
-cardValues = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King',"Ace",]
-user_card = ""
+cardValues = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace",]
+user_card = "123"
 
 #function for assigning user a card
 def first_card():
