54a52e3a71d302dcb308929322aad01750889059
Diego Gonzalez <116301465+JummyJoeJackson@users.noreply.github.com>
2023-06-05 13:00:33 -0400

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -4,7 +4,6 @@ from random import randrange
 #assign all possible card suits and values
 cardSuits = ["Spades","Clubs","Diamonds","Hearts"]
 cardValues = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace",]
-user_card = ""
 
 #gives user a random card
 user_card = cardValues[randrange(0,12)] + " of " + cardSuits[randrange(0,3)]
