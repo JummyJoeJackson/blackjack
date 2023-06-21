5201ff1957e28b7f59f99f21999262cde25e53a7
Diego Gonzalez <116301465+JummyJoeJackson@users.noreply.github.com>
2023-06-21 14:56:32 -0400

    Update blackjack.py
    -reworked check_card_val function by adding a dictionary
    -formatted all strings
    -divided check_hand_val function by making it into three separate functions

--- blackjack.py
+++ blackjack.py
@@ -2,63 +2,82 @@
 from random import randrange
 
 #declares global variables
-cardSuits = ["Spades","Clubs","Diamonds","Hearts"]
-cardValues = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace",]
-user_hand = []
-comp_hand = []
-user_hand_val = 0
-comp_hand_val = 0
+CARD_SUITS = ["Spades","Clubs","Diamonds","Hearts"]
+CARD_NAMES = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace",]
+CARD_VALUES_MAP = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "Jack": 10,
+    "Queen": 10,
+    "King": 10,
+    "Ace": 11
+}
+user = []
+comp = []
+user_val = 0
+comp_val = 0
+player_val = 0
 
 #defines hit function that gives user random card
 def hit(player):
-    card = cardValues[randrange(0,12)] + " of " + cardSuits[randrange(0,3)]
+    card = f"{CARD_NAMES[randrange(0,12)]} of {CARD_SUITS[randrange(0,3)]}"
     player.append(card)
 
 #defines function to give user their hand
-def starting_cards(player):
+def give_cards(player):
     for i in range(2):
         hit(player)
-    print("The hand is " + player[0] + " and " + player[1])
+    print(f"Your hand is {player[0]} and {player[1]}")
 
 #defines function to check user's card's value
 def check_card_val(position, player):
-    global user_hand_val
-    global comp_hand_val
-    for i in range(9):
-        if str(i + 2) in player[position]:
-            if player == user_hand:
-                user_hand_val = user_hand_val + (i + 2)
-                print("This card is worth " + str(i + 2))
-                break
-            elif player == comp_hand:
-                comp_hand_val = comp_hand_val + (i + 2)
-                print("This card is worth " + str(i + 2))
-                break
-    if "Jack" in player[position]:
-        user_hand_val = user_hand_val + 10
-        print("This card is worth 10")
-    elif "Queen" in player[position]:
-        user_hand_val = user_hand_val + 10
-        print("This card is worth 10")
-    elif "King" in player[position]:
-        user_hand_val = user_hand_val + 10
-        print("This card is worth 10")
-    elif "Ace" in player[position]:
-        user_hand_val = user_hand_val + 11
-        print("This card is worth 11")
+    global user_val
+    global comp_val
+    global player_val
+    card = player[position].split()[0]
+    player_val += CARD_VALUES_MAP[card]
+    if player == user:
+        if position == 0:
+            user_val = player_val
+            print(f"The user's first card is worth {user_val}")
+        elif position == 1:
+            user_val = player_val - user_val
+            print(f"The user's second card is worth {user_val}")
+    elif player == comp:
+        if position == 0:
+            comp_val = player_val
+            print(f"The computer's first card is worth {comp_val}")
+        elif position == 1:
+            comp_val = player_val - comp_val
+            print(f"This computer's second card is worth {comp_val}")
 
-#defines function to check and print user's total hand value
-def check_hand_val():
-    if user_hand_val == 21:
+#defines function to check if user has blackjack
+def check_blackjack():
+    if player_val == 21:
         print("YOU GOT A BLACKJACK!")
-    elif user_hand_val > 21:
+
+#defines function to check if user busted
+def check_bust():
+    if player_val > 21:
         print("YOU BUSTED!")
-        print("YOUR HAND IS WORTH " + str(user_hand_val))
-    elif user_hand_val < 21:
-        print("YOUR HAND IS WORTH " + str(user_hand_val))
+        print(f"YOUR HAND IS WORTH {player_val}")
+
+#defines function to check and print user's hand value
+def check_hand_val():
+    check_blackjack()
+    check_bust()
+    if player_val < 21:
+        print(f"YOUR HAND IS WORTH {player_val}")
 
 #executes commands
-starting_cards(user_hand)
-check_card_val(0, user_hand)
-check_card_val(1, user_hand)
+give_cards(user)
+check_card_val(0, user)
+check_card_val(1, user)
 check_hand_val()
